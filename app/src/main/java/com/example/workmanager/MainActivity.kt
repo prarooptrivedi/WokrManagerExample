899c38f04fe3fd2d8bb8a0e7df769a871780974e
praroop <praroop@galtonanalytics.com>
2020-05-18 16:58:05 +0530

    Latest Comiit

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main2.*
-
+/*Created BY Praroop*/
 class MainActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
