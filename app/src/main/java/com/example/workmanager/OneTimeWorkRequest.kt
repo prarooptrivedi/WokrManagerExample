e81045ad853ac03f94f146dd4abb3fff002ad1c4
praroop <praroop@galtonanalytics.com>
2020-05-18 12:02:28 +0530

    Latest Commit 2020_05_18-2

--- app/src/main/java/com/example/workmanager/OneTimeWorkRequest.kt
+++ app/src/main/java/com/example/workmanager/OneTimeWorkRequest.kt
@@ -3,6 +3,8 @@ package com.example.workmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,9 +19,17 @@ class OneTimeWorkRequest : AppCompatActivity() {
         }
     }
     private fun setOneTimeWorkRequest(){
+        //When assign long task like vedio Upload or other large size file upload then battery charge mandatory so use
+        //this code
+        val constraints=Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
 
         val workManager=WorkManager.getInstance(applicationContext)
        val upLoadWorkRequest=OneTimeWorkRequest.Builder(UploadWorker::class.java)
+           .setConstraints(constraints)
            .build()
         workManager.enqueue(upLoadWorkRequest)
         workManager.getWorkInfoByIdLiveData(upLoadWorkRequest.id)
